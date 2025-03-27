77c1228b9a1b30b325e8e496ec0876e5bca14003
Godzilla <1391347077@qq.com>
2025-03-27 15:43:21 +0800

    根据线上跑分

--- src/read_request_manager.cpp
+++ src/read_request_manager.cpp
@@ -168,7 +168,7 @@ bool ReadRequestManager::allocateReadRequests() {
                 // 选择要使用的副本
                 int selectedReplicaIndex = -1;
                 
-                if (loadDifference > 0.55) {
+                if (loadDifference > 0.6) {
                     // 负载差距大于40%，选择负载最小的磁盘
                     int minLoadIndex = -1;
                     int currentMinLoad = INT_MAX;
